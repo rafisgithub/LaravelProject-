59d76a409d23eb252062829fed3e9a60ed5a15ef
Rafi <smrafi120@gmail.com>
2022-12-21 20:22:17 +0600

    initial commit

--- crudInLaravelUsingAjax/app/Http/Controllers/ProductController.php
+++ crudInLaravelUsingAjax/app/Http/Controllers/ProductController.php
@@ -10,18 +10,18 @@ class ProductController extends Controller
     public function product(){
         return view('products');
     }
-    //add product
-    // public function addProduct(Request $request){
-    //     $request->validate(
-    //         [
-    //           'name'=>'required|unique:products',
-    //           'price'=>'required|unique:products',
-    //         ],
-    //         [
-    //             'name.requred'=>'Name is Required',
-    //             'name.unique'=>'Product already exists',
-    //             'price.requred'=>'price is Required',
-    //         ]
-    //         );
-    // }
+   // add product
+    public function addProduct(Request $request){
+        $request->validate(
+            [
+              'name'=>'required|unique:products',
+              'price'=>'required|unique:products',
+            ],
+            [
+                'name.requred'=>'Name is Required',
+                'name.unique'=>'Product already exists',
+                'price.requred'=>'price is Required',
+            ],
+            );
+    }
 }
